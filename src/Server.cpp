bcc3fa985235eabb60065e07e1984131dab6a376
Rahul Senna <rahulvsenna@gmail.com>
2025-07-26 16:33:00 +0530

    8: End of string anchor

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,12 +3,49 @@
 #include <ranges>
 #include <algorithm>
 
+bool match_consecutive_reverse(std::string_view input_line, const std::string_view pattern)
+{
+    for (int i = pattern.length() - 1, j = input_line.length() - 1; i >= 0; --i, --j)
+    {
+        char p = pattern[i];
+        if (p == '^')
+            return j == -1;
+
+        char chr = input_line[j];
+
+        if (p == 'd' and i > 0 and pattern[i - 1] == '\\')
+        {
+            i--;
+            if (isdigit(chr))
+                continue;
+            return false;
+        }
+        if (p == 'w' and i > 0 and pattern[i - 1] == '\\')
+        {
+            i--;
+            if (isalnum(chr) or chr == '_')
+                continue;
+            return false;
+        }
+
+        if (p == chr)
+            continue;
+
+        return false;
+    }
+    return true;
+}
+
 bool match_consecutive(std::string_view input_line, const std::string_view pattern)
 {
     for (int i = 0,j=0; i < pattern.length(); ++i, ++j)
     {
-        char chr = input_line[j];
         char p = pattern[i];
+        
+        if (p == '$')
+            return j == pattern.length();
+
+        char chr = input_line[j];
 
         if (p == chr)
             continue;
@@ -64,6 +101,11 @@ bool match_pattern(const std::string &input_line, const std::string &pattern)
     {
         return match_consecutive(input_line, pattern.substr(1));
     }
+    else if (pattern.ends_with("$"))
+    {
+        return match_consecutive_reverse(input_line, pattern.substr(0, pattern.length()-1));
+    }
+
     else
     {
         throw std::runtime_error("Unhandled pattern " + pattern);
@@ -75,6 +117,10 @@ int main(int argc, char *argv[])
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
+
+    std::string input2 = "cat";
+    std::string pattern2 = "cat$";
+    auto res = match_pattern(input2, pattern2);
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     std::cerr << "Logs from your program will appear here" << std::endl;
